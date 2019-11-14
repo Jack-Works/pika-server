3eade456b2de007e3f84fdff210ab274899db699
Jack Works <zjwpeter@gmail.com>
2019-11-14 12:45:02 +0800

    chore: better markdown style

--- demo/data.md
+++ demo/data.md
@@ -1,3 +1,4 @@
 # This is a test markdown file
 
 a [link](./)
+![Bilby Stampede](https://duckduckgo.com/assets/logo_header.alt.v108.svg)
--- src/Loaders/MarkdownLoader.ts
+++ src/Loaders/MarkdownLoader.ts
@@ -7,7 +7,23 @@ export default {
     async transformHTML(source) {
         try {
             const md = await importGlobal<typeof import('markdown-it')>('markdown-it')
-            return new md().render(source)
+            return `<meta charset="UTF-8" /><article class="markdown-body">
+${new md().render(source)}
+</article>
+<style>
+@import 'https://sindresorhus.com/github-markdown-css/github-markdown.css';
+body {
+    box-sizing: border-box;
+    min-width: 200px;
+    max-width: 980px;
+    margin: 0 auto;
+    padding: 45px;
+}
+@media (prefers-color-scheme: dark) {
+    body { background: black; filter: invert(1); }
+    img { filter: invert(1) grayscale(1); opacity: 0.6; }
+}
+</style>`
         } catch (e) {
             return msg + `<pre>` + e.message + `</pre>`
         }
