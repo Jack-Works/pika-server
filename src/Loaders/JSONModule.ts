858245747922a0e057286f597436a92e132aaa2f
Jack Works <zjwpeter@gmail.com>
2019-11-16 00:55:07 +0800

    chore: remove es import markdown

--- src/Loaders/JSONModule.ts
+++ src/Loaders/JSONModule.ts
@@ -2,6 +2,7 @@ import { Loader } from '.'
 
 export default {
     canHandle: 'application/json',
+    // TODO: deep freeze.
     transformESModule: x => `export default Object.preventExtensions(Object.freeze(${x}))`,
     transformHTML: x =>
         `<meta charset="UTF-8" /><pre><code>${JSON.stringify(JSON.parse(x), undefined, 4)}</code></pre>`,
--- src/Loaders/MarkdownLoader.ts
+++ src/Loaders/MarkdownLoader.ts
@@ -28,15 +28,4 @@ body {
             return msg + `<pre>` + e.message + `</pre>`
         }
     },
-    async transformESModule(source) {
-        try {
-            const md = new (await importGlobal<typeof import('markdown-it')>('markdown-it'))()
-            return `const p = document.createElement('p')
-p.innerHTML = ${JSON.stringify(md.render(source))}
-export default p`
-        } catch {
-            return `export default undefined
-throw new TypeError(${JSON.stringify(msg)})`
-        }
-    },
 } as Loader
